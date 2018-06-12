abf108a1b4a9c3ef19dca513159ae976d80f27d9
nevesLiliane <liliane92>
2018-06-12 20:16:37 -0300

    Adicao de variaries no example

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-from dl-steer import dt_handler , coordinator , custom_model
+from dl-steer import dt_handler , coordinator , custom_model, engine_interface,provenance
 from keras.wrappers.scikit_learn import KerasClassifier
 from spark_sklearn import GridSearchCV
 from keras.models import Sequential
+from copy import deepcopy
 
 #1. Load input data
 data = dt_handler.read_dataset('input_data.csv')
@@ -15,8 +16,12 @@ model = Sequential()
 
 ...
 
-model = KerasClassifier(build_fn=custom_model, verbose=0)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=n_jobs, scoring="accuracy")
+
+grid_search_param_grid = deepcopy(coordinator.get_task_queue()['hyperparameters'])
+grid_search_param_grid.update(dict(data))
+
+model = KerasClassifier(build_fn=custom_model.get_model(), verbose=0)
+grid = GridSearchCV(estimator=model, param_grid=grid_search_param_grid, n_jobs=-1, scoring="accuracy")
 
 queue = coordinator.get_queue()
 
@@ -24,3 +29,4 @@ X, y = data['X'], data['y']
 
 for x in queue:
 	grid_result = grid.fit(X, y)
+    provenance.persist_result(grid_result)
